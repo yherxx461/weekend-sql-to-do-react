11a63d0a12b288e385dc4be9e5d1f927aab858b3
Ying Her <yher.prime@gmail.com>
2024-01-22 06:51:24 -0600

    post data and handleSubmitTask function for form created

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -28,7 +28,9 @@ function App () {
   return (
     <div>
       <h1>TO DO APP</h1>
-      <h2></h2>
+      <div>
+      <p>{ fetchTasksList}</p>
+      </div>
     </div>
   );
 
--- src/components/addTaskForm/addTaskForm.jsx
+++ src/components/addTaskForm/addTaskForm.jsx
@@ -1,9 +1,31 @@
+import { response } from "express";
 import { useState } from useState;
 import { postTaskList } from "../addTask.api/addTask.api";
 
 function addTaskForm(props) {
     const [taskValue, setTaskValue] = useState('');
-}
+    const [statusValue, setStatusValue] = useState('');
+
+      };
+    
+      const handleSubmitTask = (event) => {
+        event.preventDefault();
+        console.log('Values for SUBMIT:', {
+          task: taskValue,
+        }); 
+      }    
+
+      // post data
+      postTaskList({
+        task: taskValue,
+        status: statusValue,
+      })
+      .then((response) => {
+        props.taskRefreshCall();
+
+        setTaskValue('');
+        setStatusValue('');
+      })
 
 return (
     <form onSubmit={handleSubmitTask}> 
@@ -12,9 +34,15 @@ return (
                 <input id="task" onChange={(event) => setTaskValue(event.target.value)} 
                 value={taskValue}
                 />
-                <button type="submit">Add Task</button>
-            </span>
-        </label>
+                     </span>
+                </label>
+                <label>
+                    <span>
+                        <input id="status" onChange={(event) => setStatusValue(event.target.value)}
+                        />
+                    </span>
+                </label>
+            <button type="submit">Add Task</button>
     </form>
 );
 
